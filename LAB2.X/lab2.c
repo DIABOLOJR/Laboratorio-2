c5e2887a20ddb86c004f3dcae7142c238a4179e0
Estuardo Mancio <man18027@uvg.edu.gt>
2020-02-03 09:04:06 -0600

    Update lab2.c
    Librerias y aumentow

--- LAB2.X/lab2.c
+++ LAB2.X/lab2.c
@@ -1,13 +1,41 @@
-/*
- * File:   lab2.c
- * Author: egman
- *
- * Created on 3 de febrero de 2020, 07:22 AM
- */
+// PIC16F887 Configuration Bit Settings
 
+// 'C' source line config statements
+
+// CONFIG1
+// Configuración del PIC
+#pragma config FOSC = INTRC_CLKOUT// Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown Out Reset Selection bits (BOR enabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
 
 #include <xc.h>
+#include <stdint.h>
 
+int a = 0;
+float b = 0;
 void main(void) {
     return;
+}
+/*
+ Codigo de incrementos
+ */
+void contador(void){
+    if (PORTDbits.RD0 =0){
+        a++;
+        PORTB = a;
+    } else if (PORTDbits.RD1 = 0){
+        a--;
+        PORTB = a;
+    }
 }
